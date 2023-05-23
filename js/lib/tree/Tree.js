7e1b940d2adeae24bd60d1b078ea5be5164c96fb
Gwentinho <qschroed@gmail.com>
2023-05-23 10:49:21 +0200

    implementation for logical tree on frontend

---
+++ js/lib/tree/Tree.js
@@ -0,0 +1,152 @@
+import TreeNode from './TreeNode.js';
+import { TreeNodeData } from './TreeNodeData.js';
+
+class Tree {
+    constructor() {
+        this.roots = [];
+        this.maxId = 0;
+    }
+
+    addNode(label, x, y, polarisation) {
+        let data = new TreeNodeData(label, x, y, polarisation, this.maxId);
+        const node = new TreeNode(data, []);
+        this.maxId++;
+
+        if (this.roots.length === 0) {
+            this.roots = [node];
+            return;
+        }
+        this.roots.push(node);
+    }
+
+    addPrimeNode(x, y) {
+        let data = new TreeNodeData("^", x, y, undefined, this.maxId);
+        const node = new TreeNode(data, []);
+        this.maxId++;
+
+        data.graph.addNode(x, y, this.maxId);
+
+        if (!this.roots?.length) {
+            this.roots = [node];
+            return;
+        }
+        this.roots.push(node);
+    }
+
+    connectPrimeNode(id1, id2) {
+        const node1 = this.getNode(id1);
+        const node2 = this.getNode(id2);
+        if (node1.data.label != "^" || node2.data.label != "^") {
+            throw "Invalid prime node connection";
+        }
+
+        node1.data.graph.addEdge(id1, id2);
+    }
+
+
+    connectRoots(id1, id2) {
+        if (id1 == id2) {
+            return;
+        }
+        const node1 = this.getRoot(id1);
+        const node2idx = this.getRootIndex(id2);
+        if (node1) {
+            const node2 = this.roots[node2idx];
+            if (node1.addChild(node2)) {
+                this.dropRootIndex(node2idx);
+            }
+        }
+
+    }
+
+    getRoot(id) {
+        for (const root of this.roots) {
+            if (root.getNode(id)) {
+                return root;
+            }
+        }
+    }
+
+    getRootIndex(id) {
+        for (let index = 0; index < this.roots.length; index++) {
+            const root = this.roots[index];
+            if (root.getNode(id)) {
+                return index;
+            }
+        }
+    }
+
+    dropRootIndex(index) {
+        this.roots.splice(index, 1);
+    }
+
+    dropRoot(id) {
+        for (let index = 0; index < this.roots.length; index++) {
+            const root = this.roots[index];
+            if (root.getNode(id)) {
+                this.roots.splice(index, 1);
+                return;
+            }
+        }
+    }
+
+    dropNode(id) {
+        for (let index = 0; index < this.roots.length; index++) {
+            const root = this.roots[index];
+            if (root.getNode(id)) {
+                root.dropNode(id);
+                if (!root.successors?.length) {
+                    this.roots.splice(index, 1);
+                }
+                return;
+            }
+        }
+    }
+
+    getNode(id) {
+        for (const root of this.roots) {
+            const node = root.getNode(id);
+            if (node != null) {
+                return node;
+            }
+        }
+    }
+
+    getPath(id) {
+        for (let index = 0; index < roots.length; index++) {
+            const root = roots[index];
+            const path = root.getPath(id);
+            if (path != null) {
+                path.unshift(index);
+                return path;
+            }
+        }
+    }
+
+    negate(id) {
+        const node = this.getNode(id);
+        node.negate();
+    }
+
+    render(cy) {
+
+        for (const root of this.roots) {
+            const renderv = root.nodeData.render(cy);
+            renderv.addClass("root");
+            root.render(cy);
+            renderv.removeClass("root");
+        }
+    }
+
+    isConnected() {
+        return this.roots.length <= 1;
+    }
+
+    serialize() {
+        if (this.isConnected()) {
+            return this.roots?.[0].serialize();
+        }
+    }
+}
+
+export default Tree;
---
+++ js/lib/tree/TreeNodeData.js
@@ -0,0 +1,134 @@
+import IdGraph from "./IdGraph.js";
+
+function isAlphaNumeric(string) {
+    return string.length == 1 && (/^[a-zA-Z0-9]/).test(string)
+};
+
+function isValidLabel(label) {
+    return isAlphaNumeric(label) || label == "&" || label == "*" || label == "^" || label == ">";
+}
+
+function toClass(string) {
+    switch (string) {
+        case "&":
+            return "par";
+        case "*":
+            return "tensor";
+        case "^":
+            return "prime";
+        case ">":
+            return "before";
+        default:
+            return "atom";
+    }
+}
+
+class TreeNodeData {
+    constructor(label, x, y, polarisation, id) {
+        if (!isValidLabel(label)) {
+            throw "Invalid label";
+        }
+        this.x = x;
+        this.y = y;
+        this.id = id;
+
+        this.class = toClass(label);
+        this.added = null;
+        this.canHaveChildren = false;
+
+        switch (label) {
+            case "&":
+                this.label = "⅋";
+                this.canHaveChildren = true;
+                this.polarisation = true;
+                break;
+            case "*":
+                this.label = "⊗";
+                this.canHaveChildren = true;
+                this.polarisation = true;
+                break;
+            case "^":
+                this.label = "prime";
+                this.graph = new IdGraph();
+                this.canHaveChildren = true;
+                this.polarisation = true;
+                break;
+            case ">":
+                this.label = "before";
+                this.canHaveChildren = true;
+                this.polarisation = true;
+                break;
+            default:
+                this.label = label;
+                this.polarisation = polarisation;
+                break;
+        }
+    }
+
+    render(cy) {
+        const node = {
+            group: 'nodes',
+            data: {
+                id: this.id,
+                label: this.label,
+                polarisation: this.polarisation,
+            },
+            renderedPosition: {
+                x: this.x,
+                y: this.y,
+            },
+        };
+        this.added = cy.add(node);
+        cy.changes.push(["add", this.added]);
+        this.added.addClass(this.class);
+        return this.added;
+    }
+
+    addClass(className) {
+        this.added.addClass(className);
+    }
+
+    removeClass(className) {
+        this.added.removeClass(className);
+    }
+
+    negate() {
+        if (!this.canHaveChildren) this.polarisation = !this.polarisation;
+    }
+
+
+    serialize() {
+        switch (this.label) {
+            case "⅋":
+                return {
+                    connective: "par",
+                    id: this.id,
+                };
+            case "⊗":
+                return {
+                    connective: "tensor",
+                    id: this.id,
+                };
+            case "prime":
+                return {
+                    connective: "prime",
+                    graph: this.graph.serialize(),
+                    id: this.id,
+                };
+            case "before":
+                return {
+                    connective: "before",
+                    id: this.id,
+                };
+            default:
+                return {
+                    connective: "atom",
+                    label: this.label,
+                    polarisation: this.polarisation,
+                    id: this.id,
+                };
+        }
+    }
+}
+
+export { TreeNodeData, isAlphaNumeric };
