26c79fecd5ea858ccba474cebac2be016e6277e5
Gwentinho <qschroed@gmail.com>
2023-05-25 23:46:24 +0200

    [feature] implemented ai rule method on frontend

--- src/main.ml
+++ src/main.ml
@@ -1,12 +1,12 @@
-open Interface.InterfaceHelpers
+open InterfaceHelpers
 open Js_of_ocaml
 
 let _ =
   Js.export_all
     (object%js
-       method getTreeJsonGS = getTreeJsonGS ()
        method decompose = decompose ()
        method recompose = recompose ()
+       method ai path1 path2 pathPar = ai_down path1 path2 pathPar
 
        method isPrime graph =
          let directed = Js.Unsafe.js_expr "directed" |> Js.to_bool in
