e9fe9deef2ae3b73b527e05e9ea4bef43a1e1830
Gwentinho <qschroed@gmail.com>
2023-05-27 20:28:51 +0200

    [feature] duplication handling and rule clicks

--- js/logic.js
+++ js/logic.js
@@ -1,15 +1,16 @@
 import { handleClick } from "./lib/handlers/clickHandler.js";
-import { cleanLayout, clearGraph, exportTree } from "./lib/util/helper.js";
+import { cleanLayout, clearGraph, exportTree, undo } from "./lib/util/helper.js";
 import { handleKeyPress } from "./lib/handlers/keypressHandler.js";
-import { handleAi, handleApply, handlePrime, handleSPar } from "./lib/handlers/ruleHandlers.js";
+import { handleAi, handlePrime, handleRuleClick, handleSPar } from "./lib/handlers/ruleHandlers.js";
 import { style } from "./lib/util/style.js";
 import Tree from "./lib/tree/Tree.js";
+import RuleHistory from "./lib/proof/RuleHistory.js";
 
 
 // Initialise global variables
 
 window.tree = new Tree();
-window.ruleHistory = [];
+window.ruleHistory = new RuleHistory();
 
 window.cy = cytoscape({
     container: document.getElementById('cy'),
@@ -17,11 +18,17 @@ window.cy = cytoscape({
     style: style
 });
 
+window.directed = false;
+
 cy.changes = [];
 
 window.mousePosition = { x: 0, y: 0 };
 
 window.isMouseOver = false;
+
+// global variables for rule application
+window.applyingRule = false;
+window.rule = null;
 
 
 // Initialise event listeners
@@ -41,34 +48,50 @@ document.addEventListener("DOMContentLoaded", event => {
 
 
 const cy_div = document.getElementById('cy');
-cy_div.addEventListener("mouseleave", function (evt) {
+cy_div.addEventListener("mouseleave", evt => {
     isMouseOver = false;
 });
-cy_div.addEventListener("mouseover", function (evt) {
+cy_div.addEventListener("mouseover", evt => {
     isMouseOver = true;
 });
 
 
 document.addEventListener('keyup', evt => {
+    // we dont want to add nodes when the mouse is not over the cy div
     if (!isMouseOver) return;
+    // we dont want to add nodes when we are applying a rule
+    if (applyingRule) return;
     handleKeyPress(cy, mousePosition, tree, evt);
 });
 
 document.getElementById("download").addEventListener("click", evt => exportTree(tree));
 document.getElementById("undo").addEventListener("click", evt => undo(cy));
 document.getElementById("clear").addEventListener("click", evt => {
-    clearGraph(cy)
+    clearGraph(cy);
+    window.applyingRule = false;
+    window.rule = null;
     window.tree = new Tree();
+    document.getElementById("selected-node-header").innerHTML = "";
+    window.ruleHistory.clear();
+    window.ruleHistory.render();
 });
 document.getElementById("center").addEventListener("click", evt => cleanLayout(cy));
 document.getElementById("import").addEventListener("click", evt => {
     document.getElementById('upload').click();
 });
 
+document.getElementById("duplicate").addEventListener("click", evt => {
+    const selected = cy.nodes(':selected');
+    if (selected.length == 1) {
+        const id = selected.id();
+        tree.duplicate(id);
+        tree.render(cy);
+    }
+});
+
 document.getElementById("ai").addEventListener("click", evt => handleAi(tree));
 document.getElementById("spar").addEventListener("click", evt => handleSPar(tree));
 document.getElementById("prime").addEventListener("click", evt => handlePrime(tree));
-document.getElementById("apply").addEventListener("click", evt => handleApply(tree));
 
 // CY EVENTS
 
@@ -79,13 +102,14 @@ cy.on('mousemove', mouseMoveEvent => {
 
 
 cy.on('click', "node", evt => {
-    handleClick(cy, tree, evt);
-});
+    // we dont want to handle clickes when the mouse is not over the cy div
+    if (!isMouseOver) return;
 
-cy.on('dblclick', "node", evt => {
-    cy.nodes().removeClass('root');
-    evt.target.addClass('root');
-    evt.target.unselect();
+    if (applyingRule) {
+        handleRuleClick(cy, evt);
+    } else {
+        handleClick(cy, tree, evt);
+    }
 });
 
 cy.on('cxttap', "node", evt => {
