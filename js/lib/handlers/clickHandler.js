a2bde3ca1fc6cdc9ee5a4f0923f54d8c02dd6475
Gwentinho <qschroed@gmail.com>
2023-05-27 20:31:23 +0200

    [chore] code cleanup

--- js/lib/handlers/clickHandler.js
+++ js/lib/handlers/clickHandler.js
@@ -1,16 +1,11 @@
-import { isAlphaNumeric, weightedBarycenter } from "../util/helper.js";
-
 function handleClick(cy, tree, evt) {
     const target = evt.target;
 
-    let selected = cy.nodes(':selected');
+    const selected = cy.nodes(':selected');
     // If there isn't exactly once selected node, do nothing
     if (selected.length != 1 || target.selected() || event.shiftKey) return;
 
     const source = selected[0];
-
-    // If source is an atom do nothing
-    if (isAlphaNumeric(source.data('label'))) return;
 
     if (source.isChild()) {
         if (target.isChild()) {
@@ -19,24 +14,20 @@ function handleClick(cy, tree, evt) {
 
             const pid = target.data('parent');
             const node = tree.getNode(pid);
-            if (node.nodeData.label === "prime") {
+            if (node.nodeData.class === "prime") {
                 // we parse the id to get the actual id of the node
                 // since the id has the "represenative" part added to it
                 node.nodeData.graph.addEdge(parseInt(source.id()), parseInt(target.id()));
                 tree.render(cy);
-                //if (added) { added.addClass(classes) };
                 source.unselect();
                 target.unselect();
                 return;
             }
 
-
-
             if (source.parent().hasClass("before")) {
                 throw "Not implemented yet";
             };
         }
-        // If the target is not a child, make the edge an outward edge and make the source a representative of the target
 
         // If the edge points from a child to its parent, do nothing
         if (target.id() == source.data('parent')) return;
@@ -51,12 +42,11 @@ function handleClick(cy, tree, evt) {
         if (target.successors()['+'](target).intersection(source.successors()['+'](source)).nonempty()) return;
 
         if (source.hasClass("before")) {
-            // not implemented yet TODO
-            return;
+            throw "Not implemented yet";
         };
 
         if (source.hasClass("prime")) {
-            // TODO rework this
+            // TODO
         };
     };
 
@@ -67,7 +57,6 @@ function handleClick(cy, tree, evt) {
     cy.batch(() => {
         tree.connectRootToNode(source.id(), target.id());
         tree.render(cy);
-        //if (added) { added.addClass(classes) };
         source.unselect();
         target.unselect();
     });
