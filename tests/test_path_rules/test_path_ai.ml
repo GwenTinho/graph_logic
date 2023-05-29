5e4e675ffb7f100ca179e37cbfd0021456561f1a
Gwentinho <qschroed@gmail.com>
2023-05-29 14:58:33 +0200

    [feature] tests for new proof model

--- tests/test_path_rules/test_path_ai.ml
+++ tests/test_path_rules/test_path_ai.ml
@@ -8,7 +8,7 @@ let%test "ai_path_test_1" =
   let p1 = [ 0; 1 ] in
   let p2 = [ 0; 0 ] in
   let pPar = [ 0 ] in
-  let res_opt = PathRules.atomic_identity_down_paths tree_arr.(0) pPar p1 p2 in
+  let res_opt = Rules.atomic_identity_down tree_arr.(0) pPar p1 p2 in
   if Option.is_none res_opt then false
   else Equality.is_empty (Option.get res_opt)
 
@@ -16,7 +16,7 @@ let%test "ai_path_test_2" =
   let p1 = [ 0; 1 ] in
   let p2 = [ 0; 0 ] in
   let pPar = [ 0 ] in
-  let res_opt = PathRules.atomic_identity_down_paths tree_arr.(0) pPar p2 p1 in
+  let res_opt = Rules.atomic_identity_down tree_arr.(0) pPar p2 p1 in
   if Option.is_none res_opt then false
   else Equality.is_empty (Option.get res_opt)
 
@@ -24,14 +24,14 @@ let%test "ai_path_test_3" =
   let p1 = [ 0; 2; 1 ] in
   let p2 = [ 0; 2; 0 ] in
   let pPar = [ 0; 2 ] in
-  let res_opt = PathRules.atomic_identity_down_paths tree_arr.(1) pPar p1 p2 in
+  let res_opt = Rules.atomic_identity_down tree_arr.(1) pPar p1 p2 in
   let p1 = [ 0; 1 ] in
   let p2 = [ 0; 0 ] in
   let pPar = [ 0 ] in
   let res_opt =
     match res_opt with
     | None -> None
-    | Some res -> PathRules.atomic_identity_down_paths res pPar p1 p2
+    | Some res -> Rules.atomic_identity_down res pPar p1 p2
   in
   if Option.is_none res_opt then false
   else Equality.is_empty (Option.get res_opt)
